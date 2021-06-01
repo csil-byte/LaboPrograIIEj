74850d30d074f07542247d523b15ccafc50f133d
Cecilia Silva <67347667+csil-byte@users.noreply.github.com>
2021-05-31 21:04:21 -0300

    commit inicial

---
+++ RepasoPrograII-cascara/RepasoPrograII-cascara/RepasoPrograII20210531/Application.Files/Xml/Xml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Application.Files.Xml
+{
+    public class Xml<T> : IFile<T>
+    {
+        //TODO implementar IFile
+        public bool Read(string file, out T data)
+        {
+            using (XmlTextReader xmlTextReader = new XmlTextReader(file))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                Xml<T> leerPersona = new Xml<T>();
+
+               if ( leerPersona.Read(file, out data))
+                {
+                    return true;
+                }
+                else
+                {
+                   return false;
+                }
+                    
+                //if (serializer.Deserialize(xmlTextReader, data))
+                //{
+                //    return true;
+                //}
+                //return (T)serializer.Deserialize(xmlTextReader);
+            }
+        }
+
+        public bool Save(string file, T data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
